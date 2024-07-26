cab3bb3d0f2af031e348e1074d561d2acf7e51f7
Saad Ahmad <saad1912@gmail.com>
2024-07-27 00:47:30 +0530

    git push origin main

---
+++ src/mlProject/entity/config_entity.py
@@ -0,0 +1,11 @@
+#Entity: Creating custom data type
+from dataclasses import dataclass
+from pathlib import Path
+
+
+@dataclass(frozen=True)
+class DataIngestionConfig:
+    root_dir: Path
+    source_url: str
+    local_data_file: Path
+    unzip_dir: Path
