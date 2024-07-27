ecf22db41e4b9b3b2d734644214f43ead5eef049
Saad Ahmad <saad1912@gmail.com>
2024-07-27 20:38:26 +0530

    git push origin main

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ app = Flask(__name__)  # initializing a flask app
 def homePage():
     return render_template("index.html")
 
-@app.route('/train', methods=['GET'])  # route to train the pipeline
+@app.route('/train', methods=['GET'])  # route to train  pipeline 
 def training():
     os.system("python main.py")
     return render_template("train_success.html")
