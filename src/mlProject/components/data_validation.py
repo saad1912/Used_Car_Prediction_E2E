8de48a347ed99a3fe984bfbeacd54ba2adb24593
Saad Ahmad <saad1912@gmail.com>
2024-07-27 11:00:17 +0530

    git push origin main

---
+++ src/mlProject/components/data_validation.py
@@ -0,0 +1,32 @@
+import os
+from mlProject import logger
+import pandas as pd
+from mlProject.entity.config_entity import DataValidationConfig
+
+
+class DataValiadtion:
+    def __init__(self, config: DataValidationConfig):
+        self.config = config
+
+    
+    def validate_all_columns(self)-> bool:
+        try:
+            validation_status = None
+
+            data = pd.read_csv(self.config.unzip_data_dir)
+            all_cols = list(data.columns)
+
+            all_schema = self.config.all_schema.keys()
+            
+
+            
+            validation_status = all(col in all_schema for col in all_cols)
+
+            # Write validation status to file
+            with open(self.config.STATUS_FILE, 'w') as f:
+                f.write(f"Validation status: {validation_status}")
+
+            return validation_status
+        
+        except Exception as e:
+            raise e
